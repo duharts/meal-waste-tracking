d3293093ae9caae10b083f27375aaaff4cbb1c51
CRF-Software <duhartd@icahnhouse.org>
2025-01-17 14:17:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from './components/ui/card';
 import { Plus, Delete } from 'lucide-react';
 
 const NumberPad = ({ value, onChange }) => {
